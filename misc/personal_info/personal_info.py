1ef98986aa0fa16f3c2a6346c8cdcdf7d8aaacf0
Aaron Adams <aaron.adams@nccgroup.com>
2020-09-03 10:15:59 +0800

    add personal stuff for interacting with forms, etc

--- misc/personal_info/personal_info.py
+++ misc/personal_info/personal_info.py
@@ -3,17 +3,31 @@ import os
 import sys
 from typing import Set
 
-from talon import Context, Module, actions, imgui, ui
+from talon import Context, Module, actions, imgui, settings, ui
 
 mod = Module()
 mod.mode("personal_info")
 mod.setting(
-    "pii_auto_select_first_entry",
+    "personal_info_auto_select",
     type=int,
     default=1,
-    desc="Auto select first entry in json list",
+    desc="Auto select specified entry in json list, or none if 0",
 )
+mod.list("personal_info", desc="List of personal info populated by json file")
 ctx = Context()
+
+
+@mod.capture
+def personal_info(m) -> list:
+    """Returns a personal_info name"""
+
+
+@ctx.capture(rule="{user.personal_info}")
+def personal_info(m):
+    return m.personal_info
+
+
+# John 11 1self1toself0
 
 # ctx.matches = r"""
 # mode: command
@@ -21,7 +35,7 @@ ctx = Context()
 
 main_screen = ui.main_screen()
 
-active_word_list = None
+personal_info_list = []
 
 
 def close_personal_info():
@@ -40,21 +54,28 @@ def gui(gui: imgui.GUI):
         index = index + 1
 
     if gui.button("Hide"):
-        close_homophones()
+        close_personal_info()
 
 
 class PersonalInfo:
     db = None
+    command_key_map = {}
 
     def __init__(self):
         cwd = os.path.dirname(os.path.realpath(__file__))
-        personal_info_file = os.path.join(cwd, "personal_info", "personal_info.json")
+        personal_info_file = os.path.join(cwd, "personal_info.json")
         with open(personal_info_file, "r") as f:
             self.db = json.loads(f.read())
+            for key in self.db:
+                self.command_key_map[" ".join(key.split("-"))] = key
+
+
+pi = PersonalInfo()
+ctx.lists["user.personal_info"] = pi.command_key_map
 
 
 def raise_personal_info():
-    actions.mode.enable("user.homophones")
+    actions.mode.enable("user.personal_info")
     gui.freeze()
 
 
@@ -66,16 +87,32 @@ class Actions:
 
     def personal_info(record: str):
         """Insert some info from the personal info database"""
+        global pi
         global personal_info_list
-        pi = PersonalInfo()
         record_data = pi.db[record]
         if type(record_data) == list:
             if len(record_data) > 1:
+
+                auto_index = settings.get("user.personal_info_auto_select")
                 personal_info_list = record_data
-                raise_personal_info()
+                if auto_index <= len(record_data):
+                    record_data = record_data[auto_index]
+                else:
+                    raise_personal_info()
             else:
                 record_data = record_data[0]
-                actions.insert(f"{record_data}")
+        actions.insert(f"{record_data}")
+
+    def personal_info_by_id(record: str, index: int):
+        """Insert some info from the personal info database"""
+        global pi
+        global personal_info_list
+        record_data = pi.db[record]
+        if type(record_data) == list:
+            if index - 1 > len(record_data):
+                index = 0
+            record_data = record_data[index - 1]
+            actions.insert(f"{record_data}")
         else:
             actions.insert(f"{record_data}")
 
--- misc/personal_info_open.talon
+++ misc/personal_info_open.talon
@@ -1,8 +1,7 @@
 mode: user.personal_info
 -
 hide: user.personal_info_hide()
-(pick | sell | cell) <number_small>:
+pick <number_small>:
     result = user.personal_info_select(number_small)
     insert(result)
     user.personal_info_hide()
-
