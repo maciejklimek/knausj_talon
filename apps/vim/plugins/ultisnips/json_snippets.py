50425a9fbd9b23833c4cf6e4819885e88980b462
Aaron Adams <aaron.adams@nccgroup.com>
2020-09-07 14:03:26 +0800

    add json support

---
+++ apps/vim/plugins/ultisnips/json_snippets.py
@@ -0,0 +1,20 @@
+from talon import Context
+
+ctx = Context()
+ctx.matches = r"""
+app: vim
+mode: user.json
+mode: command
+and code.language: json
+"""
+# spoken name -> ultisnips snippet name
+ctx.lists["user.snippets"] = {
+    "array": "a",
+    "bool": "b",
+    "named array": "na",
+    "named object": "no",
+    "null": "null",
+    "number": "n",
+    "object": "o",
+    "string": "s",
+}
