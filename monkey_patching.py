832f5245b16e8c3335c70345c391d375cd614695
Aaron Adams <aaron.adams@nccgroup.com>
2020-09-08 10:12:57 +0800

    avoid waiting for notifications

--- monkey_patching.py
+++ monkey_patching.py
@@ -1,13 +1,17 @@
 import os
+import subprocess
 
 from talon import app, ui
 
 
 def monkey_notify(body="", title="", subtitle="", *kwargs):
-    os.system('notify-send.sh -t 3000 -f -u low "%s"' % subtitle)
+    subprocess.Popen(
+        ["notify-send.sh", "-t", "3000", "-f", "-u", "low", '"%s"' % subtitle]
+    )
 
 
 def monkey_focus(self, *kwargs):
+    # subprocess.Popen(["i3-msg", f"'[class=\"(?]){self.name}\"] focus'"])
     os.system("i3-msg '[class=\"(?)%s\"] focus'" % self.name)
 
 
