e3a12f65f8c66da20df0494c40fc093a7335150e
Aaron Adams <aaron.adams@nccgroup.com>
2020-11-20 14:54:34 +0800

    finally fix annoying ui problem with monkey batch

--- monkey/monkey_patching.py
+++ monkey/monkey_patching.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-from talon import app, ui
+from talon import app, imgui, ui
 
 
 def monkey_notify(body="", title="", subtitle="", *kwargs):
@@ -15,5 +15,13 @@ def monkey_focus(self, *kwargs):
     os.system("i3-msg '[class=\"(?)%s\"] focus'" % self.name)
 
 
+# patch this to use dunst on i3wm
 app.notify = monkey_notify
+
+# patch this to use i3-msg on i3wm
 ui.App.focus = monkey_focus
+
+# on i3wm the pop up windows often minimize to and not inviewable size, with no
+# way to actually resize them. show() auto refreshes, so is more cpu intensive,
+# but does not have this problem
+imgui.GUI.freeze = imgui.GUI.show
