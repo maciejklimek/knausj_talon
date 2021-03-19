70acfc56fbd8f2ea53686ef6f9fe5bea60ee78e9
Aaron Adams <aaron.adams@nccgroup.com>
2021-03-19 12:25:08 +0800

    add useless files for now

---
+++ rpc/rpc.py
@@ -0,0 +1,22 @@
+# import tornado
+# import tornado_msgpack
+#
+# port = 18080
+#
+## dispatcher
+# dispatcher = tornado_msgpack.Dispatcher()
+#
+#
+# def add(a, b):
+#    return a + b
+#
+#
+# dispatcher.add_handler("add", add)
+#
+# server
+# server_loop = tornado.ioloop.IOLoop()
+# server = tornado_msgpack.Server(server_loop, dispatcher.on_message)
+# server.listen(port)
+
+# blocking...
+# server_loop.start()
