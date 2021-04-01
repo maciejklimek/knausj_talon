c51c0d5c21e1aa1e43fd20b6c60d402ef3cf3b36
Aaron Adams <aaron.adams@nccgroup.com>
2021-04-01 16:33:01 +0800

    add the noise code implementation with quick action stuff

---
+++ code/noise.py
@@ -0,0 +1,95 @@
+# this is adapted from @rntz script:
+# https://gist.github.com/rntz/914bdb60187858d4a014e82fbcf591c3
+
+import talon
+from talon import Module, noise, actions, scripting, ui, app
+from typing import Callable, Union, Any
+import logging
+
+mod = Module()
+@mod.action_class
+class Actions:
+
+    def pop(): 
+        """pop action overrideable by contexts"""
+        actions.user.mouse_zoom()
+
+    def pop_quick_action_clear():
+        """Clears the quick macro"""
+        global pop_quick_action
+        pop_quick_action = None
+
+    def pop_quick_action_set():
+        """Sets the quick macro"""
+        global pop_quick_action
+        if len(scripting.core.command_history) > 1:
+            pop_quick_action = scripting.core.command_history[-1]
+            logging.info(f"== quick macro set to {pop_quick_action!r} ==")
+        else:
+            logging.info(f"== command history is too short to set macro")
+
+    def pop_quick_action_run():
+        """Runs the quick macro"""
+        print(*pop_quick_action)
+        scripting.core.CoreActions.run_command(*pop_quick_action)
+
+    def hiss(): 
+        """hiss action overrideable by contexts"""
+        print("hissing")
+        pass
+
+    def hiss_quick_action_clear():
+        """Clears the quick macro"""
+        global hiss_quick_action
+        hiss_quick_action = None
+
+    def hiss_quick_action_set():
+        """Sets the quick macro"""
+        global hiss_quick_action
+        if len(scripting.core.command_history) > 1:
+            hiss_quick_action = scripting.core.command_history[-1]
+            logging.info(f"== quick macro set to {hiss_quick_action!r} ==")
+        else:
+            logging.info(f"== command history is too short to set macro")
+
+    def hiss_quick_action_run():
+        """Runs the quick macro"""
+        print(*hiss_quick_action)
+        scripting.core.CoreActions.run_command(*hiss_quick_action)
+
+
+ui.register("app_deactivate", lambda app: actions.user.pop_quick_action_clear())
+ui.register("win_focus", lambda win: actions.user.pop_quick_action_clear())
+
+pop_quick_action = None
+pop_quick_action_history = []
+def on_pop(active):
+    global pop_quick_action
+    if pop_quick_action is None:
+        actions.user.pop()
+    else:
+        actions.user.pop_quick_action_run()
+
+hiss_quick_action = None
+hiss_quick_action_history = []
+def on_hiss(active):
+    global hiss_quick_action
+    if hiss_quick_action is None:
+        actions.user.hiss()
+    else:
+        actions.user.hiss_quick_action_run()
+
+try:
+    noise.register("pop", on_pop)
+    # noise.register("hiss", on_hiss)
+except talon.lib.cubeb.CubebError as e:
+    app.notify("Failed to register pop. Is possible audio error")
+    print("Failed to register pop. Is possible audio error")
+    print(e)
+
+try:
+    noise.register("hiss", on_hiss)
+except talon.lib.cubeb.CubebError as e:
+    app.notify("Failed to register hiss. Is possible audio error")
+    print("Failed to register hiss. Is possible audio error")
+    print(e)
